893b61f088efdade7be148811fa3ef6741e22386
Tahfeez Mizan <89704978+tahfeezmizan@users.noreply.github.com>
2024-02-18 03:41:48 +0600

    responsve

---
+++ js/script.js
@@ -0,0 +1,22 @@
+// Get all the seat elements
+// const seatElements = document.querySelectorAll('.bus-sit div[id^=]');
+
+// seatElements.forEach(seat => {
+//     seat.addEventListener('click', function() {
+//         this.classList.toggle('selected');
+//     });
+// });
+
+function item() {
+    const setaNumber = document.querySelector('.item');
+    const setBg = setaNumber.classList.add('bg-green-400');
+    const setColor = setBg.classList.add('text-white');
+    const setaIncrease = document.getElementById('seat-increase');
+
+
+    console.log(setColor)
+
+    // const getSeat = document.querySelector('.item').a;
+    // getSeat.innerText.value;
+    // console.log(getSeat)
+}
