c6615ad9930f8d4b623707b9def8cf3208e7bac9
Tahfeez Mizan <89704978+tahfeezmizan@users.noreply.github.com>
2024-02-18 21:02:06 +0600

    add ticket status

--- js/script.js
+++ js/script.js
@@ -13,21 +13,23 @@ function totalSelectedSeat() {
 
 
 // Seats left function 
-function seatsLeft(){
+function seatsLeft() {
     const totalSeatElement = document.getElementById('seat-left');
     const totalSeatText = totalSeatElement.innerText;
     const totalCurrentSeat = parseInt(totalSeatText)
 
     const totalSeat = totalCurrentSeat - 1;
-    
-    totalSeatElement.innerText = totalSeat    
+    totalSeatElement.innerText = totalSeat
 }
 
 
 //add seat title 
-function seatTitle(){
+function seatTitle() {
     const seatTextName = document.querySelectorAll('.item');
 }
+
+//ticket class
+const ticketClass = 'Economy';
 
 // seat price count
 let perSeatPrice = 550;
@@ -38,14 +40,37 @@ function seatPriceCount() {
 }
 
 
+
 // get seat item
 const items = document.querySelectorAll('.item');
 
 for (const item of items) {
     item.addEventListener('click', function (key) {
-
         const itemElement = key.currentTarget;
+        const itemSerialText = key.currentTarget.innerText;
+
+        
         if (seatCount < 4) {
+            // Create container div
+            const divElement = document.createElement('div');
+            divElement.classList.add('ticket-list-container')
+
+            const p1 = document.createElement('p');
+            p1.innerText = itemSerialText;
+
+            const p2 = document.createElement('p');
+            p2.innerText = ticketClass;
+
+            const p3 = document.createElement('p');
+            p3.innerText = perSeatPrice;
+
+            divElement.appendChild(p1);
+            divElement.appendChild(p2);
+            divElement.appendChild(p3);
+
+            const ticketContainer = document.getElementById('booking-ticket-item');
+            ticketContainer.appendChild(divElement);
+
             itemElement.classList.add('bg-green-400', "text-white");
             seatCount++;
             seatsLeft()
@@ -55,6 +80,46 @@ for (const item of items) {
         else {
             alert("You can only select a maximum of 4 seats.");
         };
+
+
     })
 }
 
+// for (const item of items) {
+//     item.addEventListener('click', function (key) {
+//         const itemElement = key.currentTarget;
+//         const itemSerialText = key.currentTarget.innerText;
+
+//         if (seatCount < 4) {
+//             // Create container div
+//             const divElement = document.createElement('div');
+//             divElement.classList.add('ticket-list-container');
+
+//             const p1 = document.createElement('p');
+//             p1.innerText = itemSerialText;
+
+//             const p2 = document.createElement('p');
+//             p2.innerText = ticketClass;
+
+//             const p3 = document.createElement('p');
+//             p3.innerText = perSeatPrice;
+
+//             divElement.appendChild(p1);
+//             divElement.appendChild(p2);
+//             divElement.appendChild(p3);
+
+//             const ticketContainer = document.getElementById('booking-ticket-item');
+//             ticketContainer.appendChild(divElement);
+
+//             itemElement.classList.add('bg-green-400', "text-white");
+//             seatCount++;
+//             seatsLeft();
+//             totalSelectedSeat();
+//             seatPriceCount();
+
+
+//         } else {
+//             alert("You can only select a maximum of 4 seats.");
+//         }
+//     });
+// }
